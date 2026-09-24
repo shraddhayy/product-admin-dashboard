1282ece438d77da5a2f19bdad95af532aadfc05b
shraddha <shraddhashinde372001@gmail.com>
2026-09-24 08:34:37 +0530

    feat: add shared axios client

---
+++ src/lib/api.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://dummyjson.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token =
+      typeof window !== "undefined"
+        ? localStorage.getItem("token")
+        : null;
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error("Unauthorized request");
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
